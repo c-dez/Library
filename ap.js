f8262aae8ec8a498ea65ad68fe199870467bb3e1
cats <cats@pop-os.localdomain>
2023-07-09 23:34:42 -0600

    added js variables for inputs

--- ap.js
+++ ap.js
@@ -6,4 +6,13 @@ const library =[
         read: 'read',
     }
 ]
+
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const volume = document.querySelector('#volume')
+const read = document.querySelector('#read')
+
+const submit = document.querySelector('#submit')
+
+
    
