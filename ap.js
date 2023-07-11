3ae416e9685901125c7bef4b0cfd558f269a9e15
cats <cats@pop-os.localdomain>
2023-07-10 19:59:35 -0600

    added push obj to library array & display in table 2 valors of obj and a button

--- ap.js
+++ ap.js
@@ -21,19 +21,39 @@ function Book(title, author, volume, read){
     this.volume = volume.value;
     this.read = read.value;
 }
-let book = new Book(title, author, volume, read)
+
  
 const ul = document.querySelector('ul')
 //function creates a <li> in <ul> w text content of last item.title in array library
 function addToLibrary(){
-   let li = document.createElement('li');ul.appendChild(li)
+   let li = document.createElement('li');
+   ul.appendChild(li)
    li.textContent= library[(library.length -1)].title
 }
-addToLibrary()
+// addToLibrary()
 
-function createBook(){
-    library.push(book)
-    addToLibrary()
+function addToTable(){
+    let table = document.querySelector('table')
+    let tr = document.createElement('tr');
+    let cell = document.createElement('td');
+    let cell2 = document.createElement('td')
+    let button = document.createElement('button')
+    button.textContent = 'Erase'
+    table.appendChild(tr)
+    tr.appendChild(cell)
+    tr.appendChild(cell2)
+    tr.appendChild(button)
+    cell.textContent = library[(library.length -1)].title;
+    cell2.textContent = library[(library.length -1)].author;
 
 }
+// createBook()
+function createBook(){
+    let book = new Book(title, author, volume, read)
+    library.push(book)
+    addToTable()
+    title.value = ''
+
+}
+submit.addEventListener('click', createBook)
    
